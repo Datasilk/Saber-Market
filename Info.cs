3f04880ae0b7e0b78626ec4df8e108b2f8e03b99
Mark Entingh <entingh@gmail.com>
2021-03-14 10:57:37 -0500

    added initial files

---
+++ Info.cs
@@ -0,0 +1,13 @@
+﻿using Saber.Vendor;
+
+namespace Saber.Vendors.Market
+{
+    public class Info : IVendorInfo
+    {
+        public string Key { get; set; } = "Market";
+        public string Name { get; set; } = "Market";
+        public string Description { get; set; } = "Browse an online market of website templates that you can download & use as a starting point.";
+        public string Icon { get; set; }
+        public Version Version { get; set; } = "1.0.0.0";
+    }
+}
---
+++ Market.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Saber.Vendor;
+
+namespace Saber.Vendors.Market
+{
+    public class Market : Service, IVendorService
+    {
+        public string Filter(int categoryId, int start = 1, int length = 50, string search = "")
+        {
+            return "";
+        }
+    }
+}
